cd6f08dbf0fc25552fea1c3651eaf61775430c75
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-05-19 19:06:21 +0200

    Init.

---
+++ src/main/java/org/example/cloud/gateway/config/RoutesConfig.java
@@ -0,0 +1,38 @@
+package org.example.cloud.gateway.config;/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+class RoutesConfig {
+
+	@Bean
+	RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("demo_service_route",
+					route -> route.path("/demo-service/**")
+						.and()
+						.method(HttpMethod.GET)
+						.filters(filter -> filter.stripPrefix(1))
+						.uri("lb://demo-service"))
+			.build();
+	}
+
+}
